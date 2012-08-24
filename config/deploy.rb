71be79a9ddad162b0d08e38236881ce3d862f6ce
Ben Klein <bk.romulox@gmail.com>
2012-08-23 22:04:54 -0400

    updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "captest"
+set :repository,  "git@github.com:bklein/captest.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+server "captest.slammervanity.com", :web, :app, :db, primary: true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "bklein"
+set :deploy_to "/home/#{user}/www/#{application}"
+set :deploy_via, :remote_cache
+
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +22,19 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+  
+  task :setup_config, roles :app do
+    run "mkdir -p #{shared_path}/config"
+    puts File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Edit the files in #{shared_path}"
+  end
+  after "deploy:setup", "deploy:setup_config"
+ end
+
+
